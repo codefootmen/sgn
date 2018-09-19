5096a4ade267f7ed796fb9b08aeaec7d862d2bb2
Flavio Barci <flavio.barci@gmail.com>
2018-09-19 16:47:54 -0300

    Improve Campus class to be chainable

--- src/model/Campus.java
+++ src/model/Campus.java
@@ -1,8 +1,10 @@
 package model;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class Campus {
     private Long id;
     private String name;
