b4e80f3eefd1e58f0832d45cfa7152274cf9c2bf
Flavio Barci <flavio.barci@gmail.com>
2018-09-18 21:47:27 -0300

    Fix priority type

--- src/model/Request.java
+++ src/model/Request.java
@@ -7,6 +7,6 @@ public class Request {
     private Long id;
     private String motive;
     private String description;
-    private Priority priority;
+    private PriorityEnum priority;
     private Room room;
 }
