e54e7a3b7f8b251a2737a67972ed136efb3a3643
Felipe Toshio <felipetoshio1214@gmail.com>
2018-09-19 23:51:21 -0300

    polish Activity

--- src/model/Activity.java
+++ src/model/Activity.java
@@ -6,4 +6,7 @@ import lombok.Data;
 public class Activity {
     private Long id;
     private String name;
+    private ActivityTypeEnum activity;
+    private String area;
+    private Professor professor;
 }
