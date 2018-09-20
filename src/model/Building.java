4639fc9ae5fbd435ea16d405eaf89b6b202e0649
Filipe Jessé <filipejesse5@gmail.com>
2018-09-20 09:58:57 -0300

    Improve Building class to be chainable

--- src/model/Building.java
+++ src/model/Building.java
@@ -1,8 +1,10 @@
 package model;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class Building {
     private Long id;
     private String name;
