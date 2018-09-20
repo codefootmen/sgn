0eb92dfe31819b4c056591f5ba0c0050109af884
Filipe Jessé <filipejesse5@gmail.com>
2018-09-20 10:27:28 -0300

    Improve Course class to be chainable

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,8 +1,10 @@
 package model;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class Course {
     private Long id;
     private Boolean semester;
