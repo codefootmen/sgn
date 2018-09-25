7c465ec28b2277faa09aa3e9f4fb88a10a5771fa
Flavio Barci <flavio.barci@gmail.com>
2018-09-24 22:21:19 -0300

    Finish implementing RequestDAO functions

--- src/dao/RequestDAO.java
+++ src/dao/RequestDAO.java
@@ -4,7 +4,9 @@ import model.Request;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RequestDAO implements DAO<Request, Long> {
@@ -34,12 +36,13 @@ public class RequestDAO implements DAO<Request, Long> {
 
     @Override
     public Request find(Long key) {
-        return null;
+        String sql = String.format("SELECT * FROM request WHERE id_request = '%s'", key);
+        return search(sql).get(0);
     }
 
     @Override
     public List<Request> findAll() {
-        return null;
+        return search("SELECT * FROM request");
     }
 
     @Override
@@ -55,7 +58,7 @@ public class RequestDAO implements DAO<Request, Long> {
                     request.getPriority().toString(),
                     request.getRoom().getIdRoom(),
                     request.getIdRequest()
-                    );
+            );
             statement.execute();
             Database.closeConnection(connection, statement);
             return true;
@@ -68,6 +71,46 @@ public class RequestDAO implements DAO<Request, Long> {
 
     @Override
     public Boolean delete(Long key) {
-        return null;
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(
+                    "DELETE FROM request WHERE id_request = ?"
+            );
+            statement.setLong(1, key);
+            statement.execute();
+            Database.closeConnection(connection, statement);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return false;
+    }
+
+    private Request fromResultSet(ResultSet result) throws SQLException {
+        Request request = new Request();
+        request.setIdRequest(result.getLong("id_request"));
+        request.setMotive(result.getString("motive"));
+        request.setDescription(result.getString("description"));
+        request.setPriority(null);
+        request.setRoom(null);
+        return request;
+    }
+
+    private List<Request> search(String sql) {
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        List<Request> list = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement(sql);
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) list.add(fromResultSet(result));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return list;
     }
 }
