806e3ee5559b9554ae2182aaf59f10730b0e9c4a
Flavio Barci <flavio.barci@gmail.com>
2018-09-24 22:22:37 -0300

    Finish implementing StudentDAO functions

--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -4,7 +4,9 @@ import model.Student;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDAO implements DAO<Student, Long> {
@@ -34,12 +36,13 @@ public class StudentDAO implements DAO<Student, Long> {
 
     @Override
     public Student find(Long key) {
-        return null;
+        String sql = String.format("SELECT * FROM student WHERE id_student = '%s'", key);
+        return search(sql).get(0);
     }
 
     @Override
     public List<Student> findAll() {
-        return null;
+        return search("SELECT * FROM student");
     }
 
     @Override
@@ -68,6 +71,46 @@ public class StudentDAO implements DAO<Student, Long> {
 
     @Override
     public Boolean delete(Long key) {
-        return null;
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(
+                    "DELETE FROM student WHERE id_student = ?"
+            );
+            statement.setLong(1, key);
+            statement.execute();
+            Database.closeConnection(connection, statement);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return false;
+    }
+
+    private Student fromResultSet(ResultSet result) throws SQLException {
+        Student student = new Student();
+        student.setFirstName(result.getString("first_name"));
+        student.setLastName(result.getString("last_name"));
+        student.setEmail(result.getString("email"));
+        student.setIdStudent(result.getLong("id_student"));
+        student.setActivity(null);
+        return student;
+    }
+
+    private List<Student> search(String sql) {
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        List<Student> list = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement(sql);
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) list.add(fromResultSet(result));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return list;
     }
 }
