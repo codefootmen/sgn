4d4e3d0d056c15e8400234431569a2ee1c5bf426
Flavio Barci <flavio.barci@gmail.com>
2018-09-24 22:18:50 -0300

    Finish implementing ProgramDAO functions

--- src/dao/ProgramDAO.java
+++ src/dao/ProgramDAO.java
@@ -4,10 +4,12 @@ import model.Program;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProgramDAO implements DAO<Program, Long>{
+public class ProgramDAO implements DAO<Program, Long> {
     @Override
     public Boolean save(Program program) {
         Connection connection = Database.getConnection();
@@ -32,12 +34,13 @@ public class ProgramDAO implements DAO<Program, Long>{
 
     @Override
     public Program find(Long key) {
-        return null;
+        String sql = String.format("SELECT * FROM program WHERE id_program = '%s'", key);
+        return search(sql).get(0);
     }
 
     @Override
     public List<Program> findAll() {
-        return null;
+        return search("SELECT * FROM program");
     }
 
     @Override
@@ -64,6 +67,44 @@ public class ProgramDAO implements DAO<Program, Long>{
 
     @Override
     public Boolean delete(Long key) {
-        return null;
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(
+                    "DELETE FROM program WHERE id_program = ?"
+            );
+            statement.setLong(1, key);
+            statement.execute();
+            Database.closeConnection(connection, statement);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return false;
+    }
+
+    private Program fromResultSet(ResultSet result) throws SQLException {
+        Program program = new Program();
+        program.setIdProgram(result.getLong("id_program"));
+        program.setName(result.getString("name"));
+        program.setDepartment(null);
+        return program;
+    }
+
+    private List<Program> search(String sql) {
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        List<Program> list = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement(sql);
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) list.add(fromResultSet(result));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return list;
     }
 }
