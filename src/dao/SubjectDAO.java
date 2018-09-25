2ea968928665105082fa739c9299720832336f81
Flavio Barci <flavio.barci@gmail.com>
2018-09-24 22:23:13 -0300

    Finish implementing SubjectDAO functions

--- src/dao/SubjectDAO.java
+++ src/dao/SubjectDAO.java
@@ -4,7 +4,9 @@ import model.Subject;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SubjectDAO implements DAO<Subject, Long> {
@@ -34,12 +36,13 @@ public class SubjectDAO implements DAO<Subject, Long> {
 
     @Override
     public Subject find(Long key) {
-        return null;
+        String sql = String.format("SELECT * FROM subject WHERE id_subject = '%s'", key);
+        return search(sql).get(0);
     }
 
     @Override
     public List<Subject> findAll() {
-        return null;
+        return search("SELECT * FROM student");
     }
 
     @Override
@@ -68,6 +71,46 @@ public class SubjectDAO implements DAO<Subject, Long> {
 
     @Override
     public Boolean delete(Long key) {
-        return null;
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(
+                    "DELETE FROM subject WHERE id_subject = ?"
+            );
+            statement.setLong(1, key);
+            statement.execute();
+            Database.closeConnection(connection, statement);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return false;
+    }
+
+    private Subject fromResultSet(ResultSet result) throws SQLException {
+        Subject subject = new Subject();
+        subject.setIdSubject(result.getLong("id_subject"));
+        subject.setName(result.getString("name"));
+        subject.setQuantityOfCredits(result.getInt("quantity_of_credits"));
+        subject.setProgram(null);
+        subject.setRoomType(null);
+        return subject;
+    }
+
+    private List<Subject> search(String sql) {
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        List<Subject> list = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement(sql);
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) list.add(fromResultSet(result));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return list;
     }
 }
