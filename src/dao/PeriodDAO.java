e22e61db6d369c6c87bfc43761eac27ae5c01b27
Flavio Barci <flavio.barci@gmail.com>
2018-09-24 22:18:21 -0300

    Finish implementing PeriodDAO functions

--- src/dao/PeriodDAO.java
+++ src/dao/PeriodDAO.java
@@ -4,7 +4,9 @@ import model.Period;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PeriodDAO implements DAO<Period, Long> {
@@ -33,12 +35,13 @@ public class PeriodDAO implements DAO<Period, Long> {
 
     @Override
     public Period find(Long key) {
-        return null;
+        String sql = String.format("SELECT * FROM period WHERE id_period = '%s'", key);
+        return search(sql).get(0);
     }
 
     @Override
     public List<Period> findAll() {
-        return null;
+        return search("SELECT * FROM period");
     }
 
     @Override
@@ -66,6 +69,45 @@ public class PeriodDAO implements DAO<Period, Long> {
 
     @Override
     public Boolean delete(Long key) {
-        return null;
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(
+                    "DELETE FROM period WHERE id_period = ?"
+            );
+            statement.setLong(1, key);
+            statement.execute();
+            Database.closeConnection(connection, statement);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return false;
+    }
+
+    private Period fromResultSet(ResultSet result) throws SQLException {
+        Period period = new Period();
+        period.setIdPeriod(result.getLong("id_period"));
+        period.setStart(result.getString("start"));
+        period.setEnd(result.getString("end"));
+        period.setDayOfTheWeek(result.getString("day_of_the_week"));
+        return period;
+    }
+
+    private List<Period> search(String sql) {
+        Connection connection = Database.getConnection();
+        PreparedStatement statement = null;
+        List<Period> list = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement(sql);
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) list.add(fromResultSet(result));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Database.closeConnection(connection, statement);
+        return list;
     }
 }
