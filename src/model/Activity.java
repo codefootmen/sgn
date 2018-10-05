fb47c1897bded68c7de0854ee733733a264a8d55
Flavio Barci <flavio.barci@gmail.com>
2018-10-05 11:15:43 -0400

    Implement the proxy pattern on Activity

--- src/model/Activity.java
+++ src/model/Activity.java
@@ -1,5 +1,6 @@
 package model;
 
+import dao.*;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -11,9 +12,78 @@ public class Activity {
     private ActivityTypeEnum activityType;
     private String area;
     private Professor professor;
+    private Long idProfessor;
     private Department department;
+    private Long idDepartment;
     private Campus campus;
+    private Long idCampus;
     private Institution institution;
+    private Long idInstitution;
     private Program program;
+    private Long idProgram;
 
+    public Professor getProfessor() {
+        if(professor == null){
+            ProfessorDAO dao = new ProfessorDAO();
+            professor = dao.find(idProfessor);
+        }
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.idProfessor = professor.getIdProfessor();
+        this.professor = professor;
+    }
+
+    public Department getDepartment() {
+        if(department == null){
+            DepartmentDAO dao = new DepartmentDAO();
+            department = dao.find(idDepartment);
+        }
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.idDepartment = department.getIdDepartment();
+        this.department = department;
+    }
+
+    public Campus getCampus() {
+        if(campus == null){
+            CampusDAO dao = new CampusDAO();
+            campus = dao.find(idCampus);
+        }
+        return campus;
+    }
+
+    public void setCampus(Campus campus) {
+        this.idCampus = campus.getIdCampus();
+        this.campus = campus;
+    }
+
+    public Institution getInstitution() {
+        if(institution == null){
+            InstitutionDAO dao = new InstitutionDAO();
+            institution = dao.find(idInstitution);
+        }
+        return institution;
+    }
+
+    public void setInstitution(Institution institution) {
+        this.idInstitution = institution.getIdInstitution();
+        this.institution = institution;
+    }
+
+    public Program getProgram() {
+        if(program == null){
+            ProgramDAO dao = new ProgramDAO();
+            program = dao.find(idProgram);
+        }
+        return program;
+    }
+
+    public void setProgram(Program program) {
+        this.idProgram = program.getIdProgram();
+        this.program = program;
+    }
 }
