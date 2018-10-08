951f0a0470e2bc146c790220c9771bb829093f2c
Flavio Barci <flavio.barci@gmail.com>
2018-10-08 12:58:52 -0400

    Refactor subject to use proxy pattern

--- src/model/Subject.java
+++ src/model/Subject.java
@@ -1,15 +1,22 @@
 package model;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class Subject {
     private Long idSubject;
     private String name;
     private Integer quantityOfCredits;
-    private Program program;
     private RoomType roomType;
+    private Long idRoomType;
+    private Program program;
+    private Long idProgram;
     private Department department;
+    private Long idDepartment;
     private Campus campus;
+    private Long idCampus;
     private Institution institution;
+    private Long idInstitution;
 }
