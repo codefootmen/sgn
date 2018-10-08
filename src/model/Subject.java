657a1dd688921ec0d7681d1d4e0d5b73ac88dc8d
Flavio Barci <flavio.barci@gmail.com>
2018-10-08 13:19:45 -0400

    Finish overriding setters and getters on subject

--- src/model/Subject.java
+++ src/model/Subject.java
@@ -1,5 +1,6 @@
 package model;
 
+import dao.*;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -19,4 +20,84 @@ public class Subject {
     private Long idCampus;
     private Institution institution;
     private Long idInstitution;
+
+    public RoomType getRoomType() {
+        if (roomType == null) {
+            RoomTypeDAO dao = new RoomTypeDAO();
+            roomType = dao.findOne(idRoomType);
+        }
+        return roomType;
+    }
+
+    public Subject setRoomType(RoomType roomType) {
+        if (roomType != null) {
+            this.idRoomType = roomType.getIdRoomType();
+        }
+        this.roomType = roomType;
+        return this;
+    }
+
+    public Program getProgram() {
+        if (program == null) {
+            ProgramDAO dao = new ProgramDAO();
+            program = dao.findOne(idProgram);
+        }
+        return program;
+    }
+
+    public Subject setProgram(Program program) {
+        if (program != null) {
+            this.idProgram = program.getIdProgram();
+        }
+        this.program = program;
+        return this;
+    }
+
+    public Department getDepartment() {
+        if (department == null) {
+            DepartmentDAO dao = new DepartmentDAO();
+            department = dao.findOne(idDepartment);
+        }
+        return department;
+    }
+
+    public Subject setDepartment(Department department) {
+        if (department != null) {
+            this.idDepartment = department.getIdDepartment();
+        }
+        this.department = department;
+        return this;
+    }
+
+    public Campus getCampus() {
+        if (campus == null) {
+            CampusDAO dao = new CampusDAO();
+            campus = dao.findOne(idCampus);
+        }
+        return campus;
+    }
+
+    public Subject setCampus(Campus campus) {
+        if (campus != null) {
+            this.idCampus = campus.getIdCampus();
+        }
+        this.campus = campus;
+        return this;
+    }
+
+    public Institution getInstitution() {
+        if (institution == null) {
+            InstitutionDAO dao = new InstitutionDAO();
+            institution = dao.findOne(idInstitution);
+        }
+        return institution;
+    }
+
+    public Subject setInstitution(Institution institution) {
+        if (institution != null) {
+            this.idInstitution = institution.getIdInstitution();
+        }
+        this.institution = institution;
+        return this;
+    }
 }
