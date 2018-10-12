f851c4ad3e903de4b50c5f85aab4a355570b1359
Felipe Toshio <felipetoshio1214@gmail.com>
2018-10-12 12:34:15 -0300

    Encapsulate save, update, delete and find functions on Period class

--- src/model/Period.java
+++ src/model/Period.java
@@ -1,8 +1,12 @@
 package model;
 
+import dao.PeriodDAO;
 import dao.RoomDAO;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Data
 @Accessors(chain = true)
@@ -28,5 +32,25 @@ public class Period {
         }
         this.room = room;
         return this;
+    }
+
+    public void save() throws SQLException, ClassNotFoundException{
+        PeriodDAO.save(this);
+    }
+
+    public void update() throws SQLException, ClassNotFoundException{
+        PeriodDAO.update(this);
+    }
+
+    public void delete() throws SQLException, ClassNotFoundException{
+        PeriodDAO.delete(this);
+    }
+
+    public static Period findOne(idPeriod) throws ClassNotFoundException, SQLException{
+        return PeriodDAO.findOne(idPeriod);
+    }
+
+    public static List<Meeting> findAll() throws SQLException, ClassNotFoundException{
+        return PeriodDAO.findAll();
     }
 }
