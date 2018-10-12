3aad5e3a9a2111864cabeb68a12044f40921f80f
Felipe Toshio <felipetoshio1214@gmail.com>
2018-10-12 12:22:04 -0300

    Encapsulate save, update, delete and find functions on Institution class

--- src/model/Institution.java
+++ src/model/Institution.java
@@ -1,8 +1,12 @@
 package model;
 
 
+import dao.InstitutionDAO;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Data
 @Accessors(chain = true)
@@ -11,4 +15,24 @@ public class Institution {
     private String name;
     private String logo;
     private String site;
+
+    public void save() throws SQLException, ClassNotFoundException{
+        InstitutionDAO.save(this);
+    }
+
+    public void update() throws SQLException, ClassNotFoundException{
+        InstitutionDAO.update(this);
+    }
+
+    public void delete() throws SQLException, ClassNotFoundException{
+        InstitutionDAO.delete(this);
+    }
+
+    public static Institution findOne(idInstitution) throws ClassNotFoundException, SQLException{
+        return InstitutionDAO.findOne(idInstitution);
+    }
+
+    public static List<Institution> findAll() throws SQLException, ClassNotFoundException{
+        return InstitutionDAO.findAll();
+    }
 }
