63cd1ff5fd2ed361f82e088ebc59ade968a04641
Felipe Toshio <felipetoshio1214@gmail.com>
2018-10-13 01:01:04 -0300

    Encapsulate save, update, delete and find functions on RoomType class

--- src/model/RoomType.java
+++ src/model/RoomType.java
@@ -1,7 +1,11 @@
 package model;
 
+import dao.RoomTypeDAO;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Data
 @Accessors(chain = true)
@@ -9,5 +13,25 @@ public class RoomType {
     private Long idRoomType;
     private String name;
     private String description;
+
+    public void save() throws SQLException, ClassNotFoundException{
+        RoomTypeDAO.save(this);
+    }
+
+    public void update() throws SQLException, ClassNotFoundException{
+        RoomTypeDAO.update(this);
+    }
+
+    public void delete() throws SQLException, ClassNotFoundException{
+        RoomTypeDAO.delete(this);
+    }
+
+    public static RoomType findOne(idRoomType) throws ClassNotFoundException, SQLException{
+        return RoomTypeDAO.findOne(idRoomType);
+    }
+
+    public static List<RoomType> findAll() throws SQLException, ClassNotFoundException{
+        return RoomTypeDAO.findAll();
+    }
 
 }
