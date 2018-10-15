540c95c3b14e1c62f92759668bb7250db9814f39
Flavio Barci <flavio.barci@gmail.com>
2018-10-15 20:20:20 -0300

    Add Campus Controller

---
+++ src/controller/CampusController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import model.Campus;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CampusController", urlPatterns = {"/campi/*"})
+public class CampusController extends Servlet{
+
+
+    @Override
+    public RequestDispatcher newPage(HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public RequestDispatcher editPage(HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public RequestDispatcher showOnePage(HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public RequestDispatcher showAllPage(HttpServletRequest request) {
+        request.setAttribute("campi", Campus.findAll());
+        return request.getRequestDispatcher("/campiShowAll.jsp");
+    }
+}
