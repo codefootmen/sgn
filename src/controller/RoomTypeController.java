e2981dfca8e08364478ddf533fe0b7503a73a491
Filipe Jessé <filipejesse5@gmail.com>
2018-10-18 09:55:58 -0300

    Add RoomType Controller

---
+++ src/controller/RoomTypeController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import model.RoomType;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+
+@WebServlet(name = "RoomTypeController", urlPatterns = {"/roomTypes/*"})
+public class RoomTypeController extends Servlet {
+
+    @Override
+    public RequestDispatcher newPage(HttpServletRequest request) {
+        return request.getRequestDispatcher("/roomType/roomTypeForm.jsp");
+    }
+
+    @Override
+    public RequestDispatcher editPage(HttpServletRequest request) {
+        return request.getRequestDispatcher("/roomType/roomTypeForm.jsp");
+    }
+
+    @Override
+    public RequestDispatcher showOnePage(HttpServletRequest request) {
+        Long id = Long.valueOf(request.getAttribute("id").toString());
+        request.setAttribute("roomType", RoomType.findOne(id));
+        return request.getRequestDispatcher("/roomType/roomTypeShowOne.jsp");
+    }
+
+    @Override
+    public RequestDispatcher showAllPage(HttpServletRequest request) {
+        request.setAttribute("roomTypes", RoomType.findAll());
+        return request.getRequestDispatcher("/roomType/roomTypeShowAll.jsp");
+    }
+}
