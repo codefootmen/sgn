9ab59f84416c2bc0cee6f9156344a7a9889175e5
Filipe Jessé <filipejesse5@gmail.com>
2018-10-18 09:58:15 -0300

    Add Subject Controller

---
+++ src/controller/SubjectController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import model.Subject;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+
+@WebServlet(name = "SubjectController", urlPatterns = {"/subjects/*"})
+public class SubjectController extends Servlet {
+
+    @Override
+    public RequestDispatcher newPage(HttpServletRequest request) {
+        return request.getRequestDispatcher("/subject/subjectForm.jsp");
+    }
+
+    @Override
+    public RequestDispatcher editPage(HttpServletRequest request) {
+        return request.getRequestDispatcher("/subject/subjectForm.jsp");
+    }
+
+    @Override
+    public RequestDispatcher showOnePage(HttpServletRequest request) {
+        Long id = Long.valueOf(request.getAttribute("id").toString());
+        request.setAttribute("subject", Subject.findOne(id));
+        return request.getRequestDispatcher("/subject/subjectShowOne.jsp");
+    }
+
+    @Override
+    public RequestDispatcher showAllPage(HttpServletRequest request) {
+        request.setAttribute("subjects", Subject.findAll());
+        return request.getRequestDispatcher("/subject/subjectShowAll.jsp");
+    }
+}
