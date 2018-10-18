a3c8c22e260020d7cbe2860278c83d63ba44874a
Filipe Jessé <filipejesse5@gmail.com>
2018-10-18 09:51:08 -0300

    Add Program Controller

---
+++ src/controller/ProgramController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import model.Program;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+
+@WebServlet(name = "ProgramController", urlPatterns = {"/programs/*"})
+public class ProgramController extends Servlet {
+
+    @Override
+    public RequestDispatcher newPage(HttpServletRequest request) {
+        return request.getRequestDispatcher("/program/programForm.jsp");
+    }
+
+    @Override
+    public RequestDispatcher editPage(HttpServletRequest request) {
+        return request.getRequestDispatcher("/program/programForm.jsp");
+    }
+
+    @Override
+    public RequestDispatcher showOnePage(HttpServletRequest request) {
+        Long id = Long.valueOf(request.getAttribute("id").toString());
+        request.setAttribute("program", Program.findOne(id));
+        return request.getRequestDispatcher("/program/programShowOne.jsp");
+    }
+
+    @Override
+    public RequestDispatcher showAllPage(HttpServletRequest request) {
+        request.setAttribute("programs", Program.findAll());
+        return request.getRequestDispatcher("/program/programShowAll.jsp");
+    }
+}
