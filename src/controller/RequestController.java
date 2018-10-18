7dcf8b4e325024efe6cf8fe4b263f7dadfada7d2
Filipe Jessé <filipejesse5@gmail.com>
2018-10-18 09:53:53 -0300

    Add Request Controller

---
+++ src/controller/RequestController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import model.Request;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+
+@WebServlet(name = "RequestController", urlPatterns = {"/requests/*"})
+public class RequestController extends Servlet {
+
+    @Override
+    public RequestDispatcher newPage(HttpServletRequest request) {
+        return request.getRequestDispatcher("/request/requestForm.jsp");
+    }
+
+    @Override
+    public RequestDispatcher editPage(HttpServletRequest request) {
+        return request.getRequestDispatcher("/request/requestForm.jsp");
+    }
+
+    @Override
+    public RequestDispatcher showOnePage(HttpServletRequest request) {
+        Long id = Long.valueOf(request.getAttribute("id").toString());
+        request.setAttribute("request", Request.findOne(id));
+        return request.getRequestDispatcher("/request/requestShowOne.jsp");
+    }
+
+    @Override
+    public RequestDispatcher showAllPage(HttpServletRequest request) {
+        request.setAttribute("requests", Request    .findAll());
+        return request.getRequestDispatcher("/request/requestShowAll.jsp");
+    }
+}
