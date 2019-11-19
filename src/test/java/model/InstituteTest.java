d6eb3110260c3d14c88c6cc98db912a0641c5f7f
Flavio Barci <flavio.barci@gmail.com>
2019-11-18 22:01:26 -0300

    Add junit to project

---
+++ src/test/java/model/InstituteTest.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InstituteTest {
+    @Test
+    public void findOneTest(){
+        Assert.assertTrue(true);
+    }
+}
